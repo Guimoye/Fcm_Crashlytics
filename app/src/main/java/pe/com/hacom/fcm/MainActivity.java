f3b1c5c7024f59cca1b16466c9711371dda721c4
Javier Guimoye <javier.guimoye@hacom.com.pe>
2019-08-27 19:17:13 -0500

    fcm listo

--- app/src/main/java/pe/com/hacom/fcm/MainActivity.java
+++ app/src/main/java/pe/com/hacom/fcm/MainActivity.java
@@ -1,19 +1,26 @@
 package pe.com.hacom.fcm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public static final String TAG = MainActivity.class.getName();
     Button btn;
     List<Integer> entero= null;
 
@@ -31,5 +38,28 @@ public class MainActivity extends AppCompatActivity {
                 Crashlytics.getInstance().crash(); // Force a crash
             }
         });
+        recuperarToken();
+    }
+
+    private void recuperarToken(){
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+
+                        Log.e(TAG, token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
